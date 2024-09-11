3843d3e7f3fdc7cdd3f2114322d12a4b92d48f68
kian <vuongkhanh1537@gmail.com>
2024-09-11 17:51:49 +0700

    Init

---
+++ src/main/java/com/longhuei/pos_system_core/PosSystemCoreApplication.java
@@ -0,0 +1,13 @@
+package com.longhuei.pos_system_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSystemCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosSystemCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longhuei/pos_system_core/PosSystemCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longhuei.pos_system_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
