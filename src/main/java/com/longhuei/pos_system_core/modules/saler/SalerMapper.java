bc080a041332eb6b04d34c341b653dcb3287d4a5
mtriSE <mtri0162@gmail.com>
2024-10-18 16:41:59 +0700

    chore: typo

--- src/main/java/com/longhuei/pos_system_core/modules/saler/SalerMapper.java
+++ src/main/java/com/longhuei/pos_system_core/modules/saler/SalerMapper.java
@@ -3,11 +3,11 @@ package com.longhuei.pos_system_core.modules.saler;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.longhuei.pos_system_core.modules.saler.dto.SalerCreateDTO;
 import com.longhuei.pos_system_core.modules.saler.dto.SalerGetDTO;
+import com.longhuei.pos_system_core.modules.saler.dto.SalerSelectDTO;
 import com.longhuei.pos_system_core.modules.saler.dto.SalerUpdateDTO;
 
 @Mapper(componentModel = "spring")
